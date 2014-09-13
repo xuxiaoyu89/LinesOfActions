05857487c633530c982545457bb0f8479b1a829f
xiaoyu xu <xuxiaoyu@172-29-33-132.dynapool.nyu.edu>
2014-09-13 17:34:01 -0400

    add my logic

---
+++ LinesOfActionsLogic.js
@@ -0,0 +1,29 @@
+var isMoveOk = (function(){
+  function isEqual(object1, object2){
+    return JSON.stringify(object1) === JSON.stringify(object2);
+  }
+  
+  function copyObject(object){
+
+  }
+
+  function getWinner(){
+    
+  }
+  
+  function isTie(board){
+    
+  }
+
+  function createMove(){
+    
+  }
+  
+  function isMoveOK(params){
+
+
+  }
+
+  console.log();
+  return isMoveOk;
+})();
