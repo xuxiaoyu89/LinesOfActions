8ba41112c086b4c38c57126b38b339561224dad1
xiaoyu xu <xuxiaoyu@172-26-114-161.dynapool.nyu.edu>
2014-10-16 16:18:19 -0400

    remove

--- dtest.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('myApp', ['ngTouch', 'ngDragDrop'])
-.controller('Ctrl', function ($scope, $log) {
-  $scope.onStartCallback = function () {
-    $log.info("onStart happened!");
-  };
-});
