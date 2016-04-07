bbc9e86cadc9ad2689dbeb858e4763137f513f69
xuxiaoyu89 <xuxiaoyu89@gmail.com>
2016-04-07 16:40:13 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,17 @@
 'use strict';
-
 angular.module('myApp', ['ngDraggable', 'ngTouch'])
-  .controller('Ctrl', function (
-      $window, $scope, $log, $timeout,
-      gameService, gameLogic) {
-    
+.controller('Ctrl', function ($window, $scope, $log, $timeout,gameService, gameLogic) {
 	var moveAudio = new Audio('audio/move.wav');  
 	moveAudio.load();
-	
 	function sendComputerMove() {
-		
 		$scope.style = [[{},{},{},{},{},{},{},{}],
 		                [{},{},{},{},{},{},{},{}],
 		                [{},{},{},{},{},{},{},{}],
 		                [{},{},{},{},{},{},{},{}],
 		                [{},{},{},{},{},{},{},{}],
-				[{},{},{},{},{},{},{},{}],
-				[{},{},{},{},{},{},{},{}],
-	 			[{},{},{},{},{},{},{},{}]];
+						[{},{},{},{},{},{},{},{}],
+						[{},{},{},{},{},{},{},{}],
+	 					[{},{},{},{},{},{},{},{}]];
 		//add animate;
 		var move = gameLogic.createComputerMove($scope.board, $scope.turnIndex);
 		var brow = move[2].set.value.brow;
@@ -32,43 +26,41 @@ angular.module('myApp', ['ngDraggable', 'ngTouch'])
 	}
 	  
     function updateUI(params) {
-      //alert("hello");
-      //$scope.params = params;
-      $scope.board = params.stateAfterMove.board;
-      $scope.delta = params.stateAfterMove.delta;
-      if ($scope.board === undefined) {
-        $scope.board = gameLogic.getInitialBoard();
-        $scope.style = [[{},{},{},{},{},{},{},{}],
-		                [{},{},{},{},{},{},{},{}],
-		                [{},{},{},{},{},{},{},{}],
-		                [{},{},{},{},{},{},{},{}],
-		                [{},{},{},{},{},{},{},{}],
-				[{},{},{},{},{},{},{},{}],
-				[{},{},{},{},{},{},{},{}],
-	 			[{},{},{},{},{},{},{},{}]];
-      }
-      else{
-    	  moveAudio.play();
-      }
+      	$scope.board = params.stateAfterMove.board;
+      	$scope.delta = params.stateAfterMove.delta;
+      	if ($scope.board === undefined) {
+        	$scope.board = gameLogic.getInitialBoard();
+        	$scope.style = [[{},{},{},{},{},{},{},{}],
+			                [{},{},{},{},{},{},{},{}],
+			                [{},{},{},{},{},{},{},{}],
+			                [{},{},{},{},{},{},{},{}],
+			                [{},{},{},{},{},{},{},{}],
+							[{},{},{},{},{},{},{},{}],
+							[{},{},{},{},{},{},{},{}],
+		 					[{},{},{},{},{},{},{},{}]];
+      	}
+      	else{
+    		  moveAudio.play();
+      	}
       
-      $scope.isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
-        params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
-      $scope.turnIndex = params.turnIndexAfterMove;
-      
-      // Is it the computer's turn?
-      if ($scope.isYourTurn
-          && params.playersInfo[params.yourPlayerIndex].playerId === '') {
-        // Wait 500 milliseconds until animation ends.
-        $timeout(sendComputerMove, 750);
-      }
-    }
+	    $scope.isYourTurn = params.turnIndexAfterMove >= 0 && // game is ongoing
+	    params.yourPlayerIndex === params.turnIndexAfterMove; // it's my turn
+	    $scope.turnIndex = params.turnIndexAfterMove;
+	      
+	    // Is it the computer's turn?
+	    if ($scope.isYourTurn
+	        && params.playersInfo[params.yourPlayerIndex].playerId === '') {
+	        // Wait 500 milliseconds until animation ends.
+	        $timeout(sendComputerMove, 750);
+	    }
+	}
     
     updateUI({stateAfterMove: {}, turnIndexAfterMove: 0, yourPlayerIndex: -2});
     
     //update the start position of a move;
     $scope.onStartCallback = function(data, event, r, c){
         $scope.brow = r;
-	$scope.bcol = c;
+		$scope.bcol = c;
     	return;
     };
      
@@ -93,38 +85,33 @@ angular.module('myApp', ['ngDraggable', 'ngTouch'])
             }
             
             $scope.isYourTurn = false; // to prevent making another move
-            //$scope.firstClicked = false;
             // TODO: show animations and only then send makeMove.
-            //$log.info("before makeMove()");
             if($scope.isDragging === false){
-          	$scope.style = [[{},{},{},{},{},{},{},{}],
-              	                [{},{},{},{},{},{},{},{}],
-              	                [{},{},{},{},{},{},{},{}],
-              	                [{},{},{},{},{},{},{},{}],
-              	                [{},{},{},{},{},{},{},{}],
-              			[{},{},{},{},{},{},{},{}],
-              			[{},{},{},{},{},{},{},{}],
-              		 	[{},{},{},{},{},{},{},{}]];
-        	var brow = move[2].set.value.brow;
-          	var bcol = move[2].set.value.bcol;
-          	var arow = move[2].set.value.arow;
-          	var acol = move[2].set.value.acol;
-          	//var style = getStyle(brow, bcol, arow, acol);
-          	//$scope.style[brow][bcol] = style;
-          	//$log.info("style: ", style);
-          	$timeout(function(){
-        			  gameService.makeMove(move);
-        		  }, 500);
+	          	$scope.style = [[{},{},{},{},{},{},{},{}],
+	              	           [{},{},{},{},{},{},{},{}],
+	              	           [{},{},{},{},{},{},{},{}],
+	              	           [{},{},{},{},{},{},{},{}],
+	              	           [{},{},{},{},{},{},{},{}],
+	              			   [{},{},{},{},{},{},{},{}],
+	              			   [{},{},{},{},{},{},{},{}],
+	              		 	   [{},{},{},{},{},{},{},{}]];
+	        	var brow = move[2].set.value.brow;
+	          	var bcol = move[2].set.value.bcol;
+	          	var arow = move[2].set.value.arow;
+	          	var acol = move[2].set.value.acol;
+	          	//var style = getStyle(brow, bcol, arow, acol);
+	          	//$scope.style[brow][bcol] = style;
+	          	//$log.info("style: ", style);
+	          	$timeout(function(){
+	        		gameService.makeMove(move);
+	        	}, 500);
             }
             else gameService.makeMove(move);
             $scope.isDragging = false;
-          } catch (e) {
-            $log.info(["Cell is already full in position:", row, col]);
-            return;
-          }      
-              
-              
-              
+	    } catch (e) {
+	    	$log.info(["Cell is already full in position:", row, col]);
+	        return;
+	    }
     	return;
     };
     
